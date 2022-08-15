ead5e67ce4f7b1f749b95b3ec73385f91285b16f
Leonel Rivero <103612900+leorivero91@users.noreply.github.com>
2022-08-15 17:22:57 -0300

    Initial commit

---
+++ js/funcion.js
@@ -0,0 +1,9 @@
+let boton = document.querySelectorAll(".btn");
+function mostrar() {
+  for (var i = 0; i < boton.length; i++) {
+    boton[i].addEventListener("click", function (e) {
+      let el = this.nextElementSibling;
+      el.classList.toggle("mostrar");
+    });
+  }
+}
